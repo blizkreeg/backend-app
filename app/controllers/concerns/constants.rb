ddcdcd3f22f15e94ce3d782650bdeb72164e8ac7
Vineet Thanedar <vinthanedar@gmail.com>
2016-03-10 14:05:48 -0800

    added unmatch reason

--- app/controllers/concerns/constants.rb
+++ app/controllers/concerns/constants.rb
@@ -83,7 +83,8 @@ module Constants
     "Feels like spam",
     "Not getting replies",
     "Didn't start conversation", # TBD: run a timer to set unmatched=true and reason to this if time.now > expires_at
-    "Completed conversation" # TBD: when conversation is done/expired, set this
+    "Completed conversation", # TBD: when conversation is done/expired, set this
+    "Other person unmatched"
   ]
 
   REPORT_REASONS = [
