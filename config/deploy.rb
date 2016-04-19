81b22ba82425fd228ec88717b640bf6e09d99afb
Vineet Thanedar <vinthanedar@gmail.com>
2016-04-19 14:44:42 -0700

    updates to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :keep_assets, 2
 # set :migration_servers, -> { primary(fetch(:migration_role)) }
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.rbenv-vars')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb', '.rbenv-vars')
 
 set :bundle_without, %w{development test}.join(' ')
 set :rbenv_map_bins, %w{rake gem bundle ruby rails sidekiq sidekiqctl}
@@ -35,17 +35,17 @@ set :puma_user, fetch(:user)
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
-# set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
-set :puma_conf, "#{shared_path}/puma.rb"
+set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
+set :puma_conf, "#{shared_path}/config/puma.rb"
 set :puma_access_log, "#{shared_path}/log/puma_access.log"
 set :puma_error_log, "#{shared_path}/log/puma_error.log"
 set :puma_role, :app
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
-set :puma_threads, [1, 16]
-set :puma_workers, 0
+set :puma_threads, [2, 8]
+set :puma_workers, 2
 # set :puma_worker_timeout, nil
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
+set :puma_preload_app, false
 set :puma_init_active_record, true
 # set :nginx_use_ssl, false
 
