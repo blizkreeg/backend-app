9d02e3efa3360320a3f480f9c9db24bbeffc11f0
Vineet Thanedar <vinthanedar@gmail.com>
2016-04-20 07:32:34 -0700

    temp fix for testing

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -181,7 +181,7 @@ class Api::V1::ProfilesController < ApplicationController
   def home
     srand Time.now.to_i
 
-    @content_type = ((rand(10)%3) == 0) ? 'text' : (((rand(10)%3) == 1) ? 'link' : 'none')
+    @content_type = 'link' #((rand(10)%3) == 0) ? 'text' : (((rand(10)%3) == 1) ? 'link' : 'none')
 
     render 'api/v1/shared/home', status: 200
   end
