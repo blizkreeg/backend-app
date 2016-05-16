78288d02072fa8fb74bd3add9c7c21729f20ccdd
Vineet Thanedar <vinthanedar@gmail.com>
2016-05-15 23:45:35 -0700

    removed q_ property for thumbnail transformations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -6,7 +6,7 @@ class Photo < ActiveRecord::Base
   PUBLIC_ID_LENGTH = 10
   MAX_WIDTH = 1000
   MAX_HEIGHT = 1000
-  THUMBNAIL_TRANSFORMATIONS = 'c_fill,g_face:center,r_max,q_50'
+  THUMBNAIL_TRANSFORMATIONS = 'c_fill,g_face:center,r_max'
   PROFILE_TRANSFORMATIONS = 'c_fill,g_face:center,q_50'
   FULLSCREEN_TRANSFORMATIONS = 'c_fill,g_face:center,q_50'
 
