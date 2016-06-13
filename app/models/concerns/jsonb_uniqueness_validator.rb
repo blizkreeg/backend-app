59b2675b8ba9974ee54355177b18a1e06cb0cdf6
Vineet Thanedar <vinthanedar@gmail.com>
2016-06-13 19:33:01 +0530

    allow null emails

--- app/models/concerns/jsonb_uniqueness_validator.rb
+++ app/models/concerns/jsonb_uniqueness_validator.rb
@@ -1,5 +1,7 @@
 class JsonbUniquenessValidator < ActiveModel::EachValidator
   def validate_each(record, attr, value)
+    return if value.blank?
+
     has_record = record.class.send("with_#{attr}", value).take
     record.errors[attr] << Errors::EMAIL_EXISTS_ERROR_STR if has_record.present? && has_record.uuid != record.uuid
   end
