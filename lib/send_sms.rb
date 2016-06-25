e88a07f010938215450dfbf428a15992abd9ac44
Vineet Thanedar <vinthanedar@gmail.com>
2016-06-25 16:12:31 +0530

    added script to send sms using twilio

---
+++ lib/send_sms.rb
@@ -0,0 +1,32 @@
+require 'rubygems' # not necessary with ruby 1.9 but included for completeness
+require 'twilio-ruby'
+
+# put your own credentials here
+account_sid = 'AC21917a58e30e14acd89a6e69df6eb406'
+auth_token = '7d536102b6d6f7c417e4e75e0a84f3c3'
+
+# set up a client to talk to the Twilio REST API
+@client = Twilio::REST::Client.new account_sid, auth_token
+
+# alternatively, you can preconfigure the client like so
+Twilio.configure do |config|
+  config.account_sid = account_sid
+  config.auth_token = auth_token
+end
+
+# and then you can create a new client without parameters
+@client = Twilio::REST::Client.new
+
+numbers = %w(
+)
+
+from = 'Vineet'
+
+numbers.each_with_index do |num, idx|
+  puts "sending to #{num}"
+  response = @client.messages.create(
+    from: '+14154297272',
+    to: "+91#{num}",
+    body: "Hi! This is #{from} from ekCoffee. We are live in Mumbai! Download the ekCoffee app & get started! http://ekcoffee.com"
+  )
+end
