d0fbeebbdec5c97c51000158de5eb1ea5125f743
Vineet Thanedar <vinthanedar@gmail.com>
2017-02-04 14:49:41 -0800

    update waitlist page copy

--- app/controllers/brews_controller.rb
+++ app/controllers/brews_controller.rb
@@ -3,7 +3,7 @@ class BrewsController < WebController
 
   helper_method :live_in_user_location?
 
-  WAITLIST_LAUNCH_DATE = Date.new(2017, 1, 30)
+  WAITLIST_LAUNCH_DATE = Date.new(2017, 1, 31)
   PUBLIC_EXCEPTION_METHODS = [:show]
   WAITLIST_EXCEPTION_METHODS = [:add_to_waitlist, :show_on_waitlist, :update_phone] + PUBLIC_EXCEPTION_METHODS
   NAV_TABS_ONLY_METHODS = [:index]
@@ -109,15 +109,13 @@ class BrewsController < WebController
     # these are users who had been on the app and
     # if we are showing them a waitlist, it means they are either not approved
     # or have a low desirability score (see show_waitlist_screen?)
-    # if @current_profile.created_at <= WAITLIST_LAUNCH_DATE
-    #   flash[:message] = "#{@current_profile.firstname}, we are making some changes to \
-    #                     ekCoffee in 2017. We are rolling out these changes in a gradual fashion. We \
-    #                     have added you to our waitlist. Please bear with us while we roll this out."
-    # end
+    if live_in_user_location? && (@current_profile.created_at <= WAITLIST_LAUNCH_DATE) && @current_profile.low_desirability?
+      flash[:message] = "We are making some changes to \
+                        ekCoffee in 2017 and are rolling out these changes in a gradual fashion. We \
+                        have added you to our waitlist. Please bear with us while we roll this out."
+    end
 
-    # we've assigned this person a score and it's low so we don't want to admit them
-    # show them the total waitlist size
-    @waitlist_size = Profile.desirability_score_lte(6).count if @current_profile.low_desirability?
+    @low_score_waitlist_size = Profile.desirability_score_lte(Profile::LOW_DESIRABILITY).count
   end
 
   def update_phone
