92730cdee004a7cc33c87e9993269b1ccf4dc32f
Vineet Thanedar <vinthanedar@gmail.com>
2017-04-07 15:31:34 -0700

    clevertap update

--- lib/clevertap.rb
+++ lib/clevertap.rb
@@ -29,4 +29,30 @@ module Clevertap
       req.headers['X-CleverTap-Passcode'] = ENV['CLEVERTAP_ACCOUNT_PASSCODE']
     end
   end
+
+  # @args
+  #    event name : "App Uninstalled"
+  #    from date  : '20160601'
+  #    to date    : '20161231'
+  def profiles_by_event(name, from, to)
+    payload = { event_name: name,
+                from: from.to_i,
+                to: to.to_i
+              }.to_json
+    response = Clevertap.post_json("/1/profiles.json?batch_size=500", payload)
+    cursor =  JSON.parse(response.body)["cursor"]
+    data = []
+
+    while cursor.present? do
+      response = Clevertap.get("/1/profiles.json?cursor=#{cursor}")
+      body = JSON.parse(response.body)
+      cursor = body["next_cursor"]
+
+      next if body["records"].blank?
+
+      data += body["records"].select { |hash| hash["profileData"].present? }
+    end
+
+    data
+  end
 end
